7dc323986b62da1993709767719aa036a8ace058
d4rkc0d3r <1735522+d4rkc0d3r@users.noreply.github.com>
2022-02-20 17:39:35 +0100

    create context menu

---
+++ src/CreateAV3ToggleMenu.cs
@@ -0,0 +1,45 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using VRC.SDK3.Avatars.Components;
+
+public class CreateAV3ToggleMenu : EditorWindow
+{
+    private GameObject target;
+
+    void OnGUI()
+    {
+        target = EditorGUILayout.ObjectField("Target GameObject", target, typeof(GameObject), true) as GameObject;
+    }
+
+    public static VRCAvatarDescriptor FindAvatarDescriptor(GameObject obj)
+    {
+        VRCAvatarDescriptor descriptor = null;
+        while (!obj.TryGetComponent(out descriptor))
+        {
+            if (obj.transform.parent == null)
+                return null;
+            obj = obj.transform.parent.gameObject;
+        }
+        return descriptor;
+    }
+
+    [MenuItem("GameObject/Create AV3 Toggle", false, -1)]
+    public static void CreateAV3ToggleMenuItem()
+    {
+        var window = GetWindow(typeof(CreateAV3ToggleMenu)) as CreateAV3ToggleMenu;
+        window.target = Selection.activeObject as GameObject;
+    }
+
+    [MenuItem("GameObject/Create AV3 Toggle", true, -1)]
+    public static bool CreateAV3ToggleMenuItemValidation()
+    {
+        var obj = Selection.activeObject as GameObject;
+        if (obj == null)
+            return false;
+        return FindAvatarDescriptor(obj) != null;
+    }
+}
+#endif
